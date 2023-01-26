3af6a4903b697175d464245a890d4121c4f1d7db
Borisov Alexey <AMiBorisov@sberbank.ru>
2023-01-26 17:31:04 +0300

    Настройка k8s + InfoController, Health

---
+++ myservice/src/main/java/com/example/myservice/controller/HeaithController.java
@@ -0,0 +1,24 @@
+package com.example.myservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/health")
+public class HeaithController {
+
+    @RequestMapping(
+            value = "/sayOk",
+    method = {
+                    RequestMethod.GET,
+                    RequestMethod.POST,
+                    RequestMethod.PUT,
+                    RequestMethod.PATCH
+    })
+    public HttpStatus sayOk() {
+        return HttpStatus.OK;
+    }
+
+}
--- myservice/src/main/java/com/example/myservice/controller/InfoController.java
+++ myservice/src/main/java/com/example/myservice/controller/InfoController.java
@@ -1,5 +1,6 @@
 package com.example.myservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/info")
 public class InfoController {
 
+    @Value("${service.node.name}")
+    private String nodeName;
+
     @GetMapping(value ="/status")
     public HttpStatus getInfo() {
         return HttpStatus.OK;
+    }
+
+    @GetMapping(value = "/name")
+    public String getName() {
+        return String.format("Hello from %s", nodeName);
     }
 
 }

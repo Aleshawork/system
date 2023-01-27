22c5e393d707b1c7e1755bc35b0d3e529a142002
Alexey <borisov_alexey_2002@mail.ru>
2023-01-27 17:49:48 +0300

    k8s Deployment и LoadBalancer

--- oc-deployment/start-system-k8s.sh
+++ oc-deployment/start-system-k8s.sh
@@ -1,5 +1,8 @@
-cd oc-deployment
+cd oc-deployment/k8s/myservice
 
 kubectl create namespace myapp
 kubectl config set-context --current --namespace=myapp
+kubectl apply -f dc.yaml
+kubectl apply -f svc.yaml
+minikube tunnel
 
